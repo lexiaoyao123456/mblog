2d43fa3f40ccc9224308189ac4e805bfe0bf218e
lexiaoyao <lexiaoyao@gmail.com>
2018-08-28 01:15:12 +1000

    添加home页显示和Post列表显示

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,15 @@
+#coding=utf8
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
+
+def homepage(requeust):
+	posts = Post.objects.all()
+	post_list  = list()
+	for count, post in enumerate(posts):
+		post_list.append("No.{}:".format(str(count) + str(post) + "<br>"))
+		post_list.append("<small>" + str(post.body) + "</small><br><br>")
+		
+	return HttpResponse(post_list)
