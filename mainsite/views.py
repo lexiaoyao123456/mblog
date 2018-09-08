549334ddc73e048d40751439d4ceb578bc581557
lexiaoyao <lexiaoyao@gmail.com>
2018-09-09 02:23:53 +1000

    template

--- mainsite/views.py
+++ mainsite/views.py
@@ -2,15 +2,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from datetime import datetime
+from django.template.loader import get_template
 
 # Create your views here.
 
 def homepage(requeust):
+	template = get_template('index.html')
 	posts = Post.objects.all()
-	post_list  = list()
-
-	for count, post in enumerate(posts):
-		post_list.append("No.{} :".format(str(count) +' ' + str(post.title) + "<br>"))
-		post_list.append("<small>" + str(post.body) + "</small><br><br>")
-		
-	return HttpResponse(post_list)
+	now = datetime.now()
+	html = template.render(locals())
+	return HttpResponse(html)
